63285d262d4c4f0fb7fd42b551d2f33c80467d8a
Jérémy Berthelier <jeremy.berthelier@matera.eu>
2025-03-25 17:30:53 +0100

    ✨ feat(chuck): init chuck app

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,54 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Joke = ({ value, icon_url }) => {
+  return (
+    <>
+      <p>{value}</p>
+      <img src={icon_url} className="chuck" />
+    </>
+  )
+}
 
-const Card = ({title}: {title: string}) => {
+const RefreshJoke = ({ onClick }) => {
+  return (
+    <div>
+      <button onClick={onClick}>Refresh</button>
+    </div>
+  )
+}
 
-  
- 
-  
-  return <div>{title}</div>
+const Loader = () => {
+  return (
+    <a href="https://react.dev" target="_blank">
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </a>
+  )
 }
 
 function App() {
-  const [count, setCount] = useState({
-    count_1: 0,
-    count_2: 0,
-  })
-  const [count2, setCount2] = useState(0)
+  const [loading, setLoading] = useState(true)
+  const [joke, setJoke] = useState()
 
+  const fetchJoke = async () => { 
+    const url = "https://api.chucknorris.io/jokes/random"
+    const response = await fetch(url)
+    setJoke(await response.json())
+    setLoading(false)
+  }
 
   useEffect(() => {
-    console.log('mount')
-    
-    return () => console.log('unmount')
-  }, [count])
+    setTimeout(fetchJoke, 2000)
+  }, [])
+
+
   
   console.log('App re-render')
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((prev) => ({...prev, count_1: prev.count_1 + 1}))}>
-          count is {count.count_1}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      {count.count_1 >= 1 && <Card title={`le compte est de ${count}`}/>}
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading && <Loader /> }
+      {joke && <Joke value={joke.value} icon_url={joke.icon_url}/>}
+      {joke && <RefreshJoke onClick={fetchJoke} />}
     </>
   )
 }
